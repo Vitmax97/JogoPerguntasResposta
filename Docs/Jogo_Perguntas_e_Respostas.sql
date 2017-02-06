27546beb3df9827fc86c90de41897def2c9835f9
Vitor <aresplayer@outlook.com>
2017-02-06 17:30:04 -0200

    Aula 06/02/2017 Guto

--- Docs/Jogo_Perguntas_e_Respostas.sql
+++ Docs/Jogo_Perguntas_e_Respostas.sql
@@ -1,13 +1,35 @@
-create database db_PerguntasERespostas
+create database db_PerguntasERespostas --Cria um database--
 
-use db_PerguntasERespostas
+use db_PerguntasERespostas --começa a usar o database--
 
-create table tb_Jogador(
+create table tb_Jogador(              --Começo de criar uma tabela--
 	id int identity primary key,
 	nome varchar(100) not null
-)
+)                                     --Término da criação da tabela--
 
-select * from tb_Jogador
+truncate table tb_Jogador --Reseta o ID da tabela--
 
-insert into tb_Jogador(nome) values('Amanda')
-insert into tb_Jogador(nome) values('Fernanda')
+select * from tb_Jogador --Seleciona tudo da tabela--
+
+delete from tb_Jogador where id = 9 --Deleta o ID 9 da tabela--
+
+insert into tb_Jogador(nome) values('Ramón')--Insere o nome na tabela--
+
+-------------------------------------------------------------------------------
+
+create table tb_Perguntas (                --Começo da criação da tabela--
+id_Perguntas int identity primary key,
+pergunta varchar(max),
+resposta_correta varchar(max),
+data datetime default getdate()
+
+)											--Término da criação da tabela--
+
+select * from tb_Perguntas
+
+insert into tb_Perguntas(pergunta, resposta_correta) --Insere na tabela tb_Perguntas as variáveis pergunta e resposta_correta--
+values ('Qual é a cor do cavalo branco de napoleão?', 'Branco') --Insere na tabela a pergunta e a resposta correta--
+
+delete from tb_Jogador --Deleta os nomes da tabela--
+
+alter table tb_Perguntas add id_jogador int
--- JogoPerguntasRespostas/JogoDesktop/Pergunta1.cs
+++ JogoPerguntasRespostas/JogoDesktop/Pergunta1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,14 @@ using System.Windows.Forms;
 namespace JogoDesktop
 {
     public partial class Pergunta1 : Form
+
     {
-        public Pergunta1()
+        public int id_jogador_banco;
+
+        public Pergunta1(int id_jogador)
         {
             InitializeComponent();
+            id_jogador_banco = id_jogador;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -32,12 +37,30 @@ namespace JogoDesktop
             if (rdbResposta3.Checked == true)
             {
                 MessageBox.Show("Acertou!");
+                //início do código para inserir o jogador na tabela
+                //using System.Data.SqlClient;
+                using (SqlConnection conexao = new SqlConnection("Server=AME0556318W10-1\\SQLEXPRESS;Database=db_PerguntasERespostas;Trusted_Connection=Yes"))
+                {
+                    using (SqlCommand comando = new SqlCommand("insert into tb_Perguntas(pergunta, resposta_correta, id_jogador) values(@PERGUNTA, @RESPOSTA3, @ID_JOGADOR)", conexao))
+                    {
+                        comando.Parameters.AddWithValue("PERGUNTA", lblPergunta.Text);
+                        comando.Parameters.AddWithValue("RESPOSTA3", rdbResposta3.Text);
+                        comando.Parameters.AddWithValue("ID_JOGADOR", id_jogador_banco);
+                        conexao.Open();
+                        comando.ExecuteNonQuery();
+
+
+
+                    }
+                }
+
             }
             else
             {
                 MessageBox.Show("Errou");
 
             }
+            
         }
     }
 }
